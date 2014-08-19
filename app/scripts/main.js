03514ab25f51587efe9945ea9669ce31f21e9df4
David Wilcox <dave@davidgwilcox.com>
2014-08-19 13:15:08 +1000

    hello world using revealjs

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+$(function(){
+  setTimeout(main, 3000);
+});
+
+function main(){
+  Reveal.initialize({
+    controls: true,
+    slideNumber: true,
+    history: true,
+    keyboard: true,
+    overview: true,
+    center: false,
+    touch: true,
+    hideAddressBar: true,
+    transition: 'default'
+  });
+}
