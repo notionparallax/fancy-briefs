bb1f950d61d7265d6854b89746e79a22a99b893c
David Wilcox <dave@davidgwilcox.com>
2014-08-19 13:48:42 +1000

    working query to test spreadsheet

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,7 +2,18 @@ $(function(){
   setTimeout(main, 3000);
 });
 
-function main(){
+function loadContent(){
+  return $.getJSON("https://spreadsheets.google.com/feeds/list/1xPj-1ZVF1K4Y5kvdRLH8V9UTqkfr2vUFesysNeMtg7U/1/public/basic?alt=json");
+}
+
+function processContent(googleSpreadsheet){
+
+}
+
+function applyTemplate(){
+}
+
+function initReveal() {
   Reveal.initialize({
     controls: true,
     slideNumber: true,
@@ -14,4 +25,11 @@ function main(){
     hideAddressBar: true,
     transition: 'default'
   });
+}
+
+function main(){
+  loadContent()
+    .then(processContent)
+    .then(applyTemplate)
+    .then(initReveal);
 }
